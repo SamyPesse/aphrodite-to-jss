30d7916d17ff4831890278d699eb551cfb5abb44
Samy Pessé <samypesse@gmail.com>
2018-12-30 15:51:28 +0100

    Fix all errors

--- src/css.ts
+++ src/css.ts
@@ -6,45 +6,48 @@ import { SheetDefinition, SheetDefinitions } from './types';
  * Compute a className from a set of sheet definitions.
  */
 function css(...args: SheetDefinitions): string {
-    const rules = flattenRules(args);
-    const className = rules.map(rule => rule.className).join('--');
+  const rules = flattenRules(args);
+  const className = rules.map(rule => rule.className).join('--');
 
-    if (StyleSheet.sheet.getRule(className)) {
-        return className;
-    }
-
-    const style = rules.reduce(
-        (result, rule) => mergeStyles(result, rule.style),
-        {}
-    );
-
-    rules.forEach(rule => {
-        Object.keys(rule.extras).forEach(extraKey => {
-            StyleSheet.sheet.addRule(extraKey, rule.extras[extraKey]);
-        });
-    });
-
-    StyleSheet.sheet.addRule(className, style, { selector: `.${className}` });
-
+  if (StyleSheet.sheet.getRule(className)) {
     return className;
-}
+  }
 
+  const style = rules.reduce(
+    (result, rule) => mergeStyles(result, rule.style),
+    {}
+  );
+
+  rules.forEach(rule => {
+    Object.keys(rule.extras).forEach(extraKey => {
+      StyleSheet.sheet.addRule(extraKey, rule.extras[extraKey]);
+    });
+  });
+
+  // @ts-ignore RuleOptions can be only { selector: string }
+  StyleSheet.sheet.addRule(className, style, { selector: `.${className}` });
+
+  return className;
+}
 
 /*
  * Flatten a nested SheetDefinitions into a list of SheetDefinition.
  */
 function flattenRules(rules: SheetDefinitions): SheetDefinition[] {
-    return rules.reduce((result: SheetDefinition[], rule: SheetDefinition | SheetDefinitions) => {
-        if (!rule) {
-            return result;
-        }
+  return rules.reduce(
+    (result: SheetDefinition[], rule: SheetDefinition | SheetDefinitions) => {
+      if (!rule) {
+        return result;
+      }
 
-        if (rule instanceof Array) {
-            return result.concat(flattenRules(rule));
-        }
+      if (rule instanceof Array) {
+        return result.concat(flattenRules(rule));
+      }
 
-        return result.concat([rule]);
-    }, []);
+      return result.concat([rule]);
+    },
+    []
+  );
 }
 
 export default css;
--- src/stylesheet.ts
+++ src/stylesheet.ts
@@ -1,5 +1,5 @@
 import { create, StyleSheet } from 'jss';
-import { StyleDefinitions } from './types';
+import { SheetDefinition, StyleDefinitions } from './types';
 
 import normalizeStyle from './normalizeStyle';
 import { hash } from './utils';
@@ -7,71 +7,81 @@ import { hash } from './utils';
 const jss = create();
 
 const StyleSheet = {
-    toCSSString,
-    create: createStyleSheet,
-    reset: resetStyleSheet,
-    attach: attachStyleSheet,
-    sheet: createSheet(),
-    globalSheet: createSheet()
-}
+  toCSSString,
+  create: createStyleSheet,
+  reset: resetStyleSheet,
+  attach: attachStyleSheet,
+  sheet: createSheet(),
+  globalSheet: createSheet()
+};
 
-function createStyleSheet(input: StyleDefinitions) {
-    return Object.keys(input).reduce((map, name) => {
-        const { style, extras } = normalizeStyle(input[name]);
+function createStyleSheet(
+  input: StyleDefinitions
+): { [key: string]: SheetDefinition } {
+  return Object.keys(input).reduce(
+    (map: { [key: string]: SheetDefinition }, name: string) => {
+      const { style, extras } = normalizeStyle(input[name]);
 
-        if (name === '@global') {
-            StyleSheet.globalSheet.addRule(name, style);
+      if (name === '@global') {
+        StyleSheet.globalSheet.addRule(name, style);
 
-            Object.keys(extras).forEach(extraKey => {
-                StyleSheet.globalSheet.addRule(extraKey, extras[extraKey]);
-            });
-        }
+        Object.keys(extras).forEach(extraKey => {
+          StyleSheet.globalSheet.addRule(extraKey, extras[extraKey]);
+        });
+      }
 
-        const className = generateClassName(name, style);
+      const className = generateClassName(name, style);
 
-        map[name] = {
-            className,
-            extras,
-            style
-        };
-        return map;
-    }, {});
+      map[name] = {
+        className,
+        extras,
+        style
+      };
+      return map;
+    },
+    {}
+  );
 }
 
 function generateClassName(name: string, style: any): string {
-    return `${name}-${hash(style)}`;
+  return `${name}-${hash(style)}`;
 }
 
 /*
  * Create the JSS stylesheet.
  */
 function createSheet(): StyleSheet {
-    return jss.createStyleSheet({}, {
-        meta: 'aphrodite-to-jss'
-    });
+  return jss.createStyleSheet(
+    {},
+    {
+      meta: 'aphrodite-to-jss'
+    }
+  );
 }
 
 /*
  * For SSR: Return all of the CSS generated since the last "resetStyles".
  */
 function toCSSString() {
-    return [StyleSheet.globalSheet.toString(), StyleSheet.sheet.toString()].join('\n\n');
+  return [StyleSheet.globalSheet.toString(), StyleSheet.sheet.toString()].join(
+    '\n\n'
+  );
 }
 
 /*
  * For Browser: Attach the stylesheet to the DOM.
  */
 function attachStyleSheet() {
-    StyleSheet.globalSheet.attach();
-    StyleSheet.sheet.attach();
+  StyleSheet.globalSheet.attach();
+  StyleSheet.sheet.attach();
 }
 
 /*
  * For SSR: Reset everything.
  */
 function resetStyleSheet() {
-    jss.removeStyleSheet(StyleSheet.sheet);
-    StyleSheet.sheet = createSheet();
+  jss.removeStyleSheet(StyleSheet.sheet);
+  StyleSheet.sheet = createSheet();
 }
 
 export default StyleSheet;
